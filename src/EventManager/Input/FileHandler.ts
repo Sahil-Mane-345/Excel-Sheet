4abf980d5403e536c37fc5d113081b8f5c0a6b71
Sahil-Mane-345 <sahil.mane@zeuslearning.com>
2026-07-22 15:25:26 +0530

    Added File export feature

--- src/EventManager/Input/FileHandler.ts
+++ src/EventManager/Input/FileHandler.ts
@@ -5,15 +5,16 @@ export class FileHandler {
 
     private fileInput: HTMLInputElement;
 
+    private exportButton: HTMLButtonElement;
+
     constructor(
         private cellEditor: CellEditor,
         private renderer: Renderer
     ) {
 
-        this.fileInput =
-            document.getElementById(
-                "file-input"
-            ) as HTMLInputElement;
+        this.fileInput = document.getElementById("file-input") as HTMLInputElement;
+
+        this.exportButton = document.getElementById("export-sheet") as HTMLButtonElement;
 
     }
 
@@ -29,9 +30,12 @@ export class FileHandler {
                     console.warn("No file selected.");
                     return;
                 }
+                console.log(file.name);
+                console.log(file.name.toLowerCase().endsWith(".json"));
+                console.log(file.name.toLowerCase().endsWith(".sheet"));
 
-                if (!file.name.toLowerCase().endsWith(".json")) {
-                    alert("Please select a valid JSON file.");
+                if (!file.name.toLowerCase().endsWith(".json") && !file.name.toLowerCase().endsWith(".sheet")) {
+                    alert("Please select a valid JSON/sheet file.");
                     target.value = "";
                     return;
                 }
@@ -56,12 +60,47 @@ export class FileHandler {
             }
         );
 
+        this.exportButton.addEventListener("click", () => this.exportSheet());
+
     }
 
     public importSheet(json: string): void {
         const sheet = JSON.parse(json);
-        this.cellEditor.loadRecords(sheet);
+        if(Array.isArray(sheet)){
+            this.cellEditor.loadRecords(sheet);
+        }else if(
+            typeof sheet === "object" && sheet!== null && sheet.type === "sheet" && typeof sheet.cells === "object" 
+        ){
+            this.cellEditor.importCells(sheet.cells);
+        }
         this.renderer.render();
+    }
+
+    public exportSheet(): void {
+        const sheet = {
+            type: "sheet",
+            version: 1,
+            cells: Object.fromEntries(this.cellEditor.getAllCells())
+        };
+
+        const json = JSON.stringify(sheet, null, 2);
+
+        const blob = new Blob([json], {
+            type: "applicaion/json"
+        });
+
+        const url = URL.createObjectURL(blob);
+
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${Date.now()}.sheet`;
+        link.classList = "hidden";
+        
+        link.click();
+        document.body.appendChild(link);
+        document.body.removeChild(link);
+
+        URL.revokeObjectURL(url);
     }
 
 }
